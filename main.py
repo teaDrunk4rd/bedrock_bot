5fc62c13b7d8f00df36a5f13269bf5c668f5cca8
Руслан Шамсиев <pizdos.s@mail.ru>
2020-07-02 01:01:42 +0500

    +memes, answer_rules

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.longpoll import VkEventType
 from config import Config
 from vk_methods import Vk
 
-abuses = ['ебал', 'гандон', 'пидор', 'хуй', 'соси', 'мразь']
+admin_ids = [132228887, 322270793]
+abuses = ['ебал', 'гандон', 'пидор', 'хуй', 'соси', 'мразь', 'залупа', 'жопа', 'член', 'еблан', 'вагина', 'долбаеб']
+answer_rules = {
+    'ты пидор': lambda id: vk.send_message_sticker(id, 'а может ты пидор?', 49)
+}
 
 vk = Vk(vk_api.VkApi(token=Config.token))
 
 for event in vk.longpool.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         message = event.text.lower()
-        if any([abuse for abuse in abuses if abuse in message]):
-            if event.user_id == 132228887:
-                vk.send(event.user_id, 'Хозяин, не нужно мне такие гадости писать, мне неприятно. Лучше склепайте новых мемов.')
+        if any([rule for rule in answer_rules if rule in message]):
+            answer_rules[[rule for rule in answer_rules if rule in message][0]](event.user_id)
+        elif any([abuse for abuse in abuses if abuse in message]):
+            if event.user_id in admin_ids:
+                vk.send_message_sticker(event.user_id, 'хозяин, не нужно мне такие гадости писать, мне неприятно. лучше склепайте новых мемов')
             else:
-                vk.send(event.user_id, f'Мразь, я знаю что ты сидишь с id {event.user_id}, я тебя найду и уничтожу.')
+                vk.send_message_sticker(event.user_id, f'мразь, я знаю что ты сидишь с id {event.user_id}, я тебя найду и уничтожу', 62)
         elif 'привет' in message:
-            if event.user_id == 132228887:
-                vk.send(event.user_id, 'Здравствуйте, хозяин.')
+            if event.user_id in admin_ids:
+                vk.send(event.user_id, 'здравствуйте, хозяин')
             else:
-                vk.send(event.user_id, f'И тебя приветствую.')
+                vk.send(event.user_id, f'и тебя приветствую')
--- vk_methods.py
+++ vk_methods.py
@@ -2,18 +2,37 @@ from vk_api.longpoll import VkLongPoll
 
 
 class Vk:
-    vk_session = None
+    session = None
     session_api = None
     longpool = None
 
     def __init__(self, vk_session):
-        self.vk_session = vk_session
-        self.session_api = self.vk_session.get_api()
+        self.session = vk_session
+        self.session_api = self.session.get_api()
         self.longpool = VkLongPoll(vk_session)
 
+    # def send_message(self, id, text=None, sticker_num=None):
+    #     self.session.method('messages.send', {
+    #         'user_id': id,
+    #         'message': text,
+    #         'sticker_id': sticker_num,
+    #         'random_id': 0
+    #     })
+
     def send(self, id, text):
-        self.vk_session.method('messages.send', {
+        self.session.method('messages.send', {
             'user_id': id,
             'message': text,
             'random_id': 0
         })
+
+    def send_sticker(self, id, num):
+        self.session.method('messages.send', {
+            'user_id': id,
+            'sticker_id': num,
+            'random_id': 0
+        })
+
+    def send_message_sticker(self, id, text, sticker_num):
+        self.send(id, text)
+        self.send_sticker(id, sticker_num)
